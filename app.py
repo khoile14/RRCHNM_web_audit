41e85217c0172b98c977ea70bc10a9e38a76319f
Khoi Le <khoile1041@gmail.com>
2023-07-19 02:28:50 -0400

    need to fix some of the UI features

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 import requests
-import json
 
 app = Flask(__name__)
 
@@ -22,16 +21,9 @@ def index():
     if request.method == 'POST':
         links = request.form.get('text').split('\n')
         results = check_links(links)
-        serialized_results = json.dumps(results)  # Serialize the results to JSON
-        return redirect(url_for('show_results', results=serialized_results))
+        return jsonify(results)
     else:
         return render_template('index.html')
-
-@app.route('/results')
-def show_results():
-    serialized_results = request.args.get('results')
-    results = json.loads(serialized_results) if serialized_results else None  # Deserialize the results from JSON
-    return render_template('results.html', results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
